e91742405334335757c87b72126eae8270669d64
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-07-30 00:52:49 -0300

    Update armario.hs

--- armario.hs
+++ armario.hs
@@ -12,33 +12,46 @@ consulta :: Int -> Armarios -> Maybe Estado
 consulta indice armarios =
   case Map.lookup indice armarios of 
      Nothing-> Nothing
-     Just Armario ->  Armario (Estado)
+     Just (estado,codigo) -> Just estado
 
 -- dado o número do armário, retorna Right armário ou Left "Armario nao existe"
 pegarArmario :: Int -> Armarios -> Either String Armario
 pegarArmario indice armarios = 
-  case Map.lookup indice armarios of=
-   Nothing-> Left $ "armario " ++ indice ++ " não existe"
-   Just Armario -> Right $ "armario " ++ indice ++ " esta" ++ Armario (Estado)
+  case Map.lookup indice armarios of
+   Nothing-> Left $ "armario " ++ show indice ++ " nao existe"
+   Just armario -> Right armario
 
 -- consultar: dado o número de um armário retorna "Right código"
 -- Se não existir, retorna "Left Armario não existe", se já estiver alugado retorna "Left ja esta ocupado"
 -- Utilize a função pegarArmario para obter o armário e verificar se ele existe
 -- Não reescreva a mesma mensagem de erro, utilize a invocação do pegarArmário
 pegarCodigo :: Int -> Armarios -> Either String Codigo
-pegarCodigo indice armarios = ...
+pegarCodigo indice armarios =
+  case pegarArmario indice armarios of
+    Left string->  Left string
+    Right (estado, codigo) -> if estado /= Alugado
+     then Right codigo
+     else Left $ "armario "++ show indice++ " ja esta ocupado"
 
 
 -- aluguel - se o armário existir e estiver desocupado, o estado do armário no mapa é alterado para alugado
 -- reaproveite a função pegarCodigo para obter a chave atual e os erros de "não existe" e "está ocupado"
 aluguel :: Int -> Armarios -> Either String Armarios
-aluguel indice armarios = ...
+aluguel indice armarios = 
+  case pegarArmario indice armarios of
+    Right (estado,codigo)  -> Right (Map.insert indice (Alugado,codigo) armarios)
+    Left string -> Left string
 
 -- para devolver um armário é necessário que ele exista, que esteja alugado e que o código esteja correto
 -- utilize a função pegar armário para verificar a existência.
 -- Se o armário não estiver alugado ou se o código estiver incorreto avise utilizando o Left.
 devolucao :: Int -> Codigo -> Armarios -> Either String Armarios
-devolucao indice codigo armarios =  ...
+devolucao indice codigo armarios = 
+  case pegarArmario indice armarios of
+    Left string -> Left string
+    Right (estado,codigo1) -> if estado /= Livre 
+                     then if codigo1==codigo then Right (Map.insert indice (Livre,codigo1) armarios) else Left $ "codigo incorreto"
+                     else Left $ "armario "++ show indice++ " esta desocupado"
 
 
 lockers :: Armarios
